f590823b7ff7d67905f59cef0efdffb4cd520326
kasandradarwin <86120308+kasandradarwin@users.noreply.github.com>
2022-12-06 15:59:19 -0800

    added some new components, delete option

--- src/App.js
+++ src/App.js
@@ -1,12 +1,55 @@
 import './App.css';
 import Header from "./components/Header"
 import { Tasks } from './components/Tasks';
+import { useState } from 'react'
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+     id:1,
+     text: 'Doctors Appointment',
+     day: 'Feb 5th at 3:30pm',
+     reminder: true, 
+    },
+
+{
+    id:2,
+    text: 'Groceries',
+    day: 'Feb 5th at 1:30pm',
+    reminder: true, 
+    },
+
+    {
+    id:3,
+    text: 'Meeting',
+    day: 'Feb 5th at2:00pm',
+    reminder: true, 
+    },
+
+    {
+    id:4,
+    text: 'Ballet',
+    day: 'Feb 5th at 2:30pm',
+    reminder: false, 
+    },
+
+])
+
+//Delete Task
+
+const deleteTask =(id) => {
+  setTasks(tasks.filter(() => tasks.id !==id))
+}
+
+//toggle reminder 
+
+const toggleReminder = (id) => {
+  setTasks(tasks.map((task) => task.id === id ? {} : task))
+}
   return (
     <div className="App">
       <Header />
-      <Tasks />
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> :  'No Tasks To Show'}
 
     </div>
   );
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,39 +1,13 @@
-import { useState } from 'react'
+import Task from './Task'
 
-export const Tasks = () => {
-    const [tasks, setTasks] = useState([
-        {
-         id:1,
-         text: 'Doctors Appointment',
-         day: 'Feb 5th at 3:30pm',
-         reminder: true, 
-        },
+export const Tasks = ({ tasks, onDelete, onToggle }) => {
     
-    {
-        id:2,
-        text: 'Groceries',
-        day: 'Feb 5th at 1:30pm',
-        reminder: true, 
-        },
-    
-        {
-        id:3,
-        text: 'Meeting',
-        day: 'Feb 5th at2:00pm',
-        reminder: true, 
-        },
-    
-        {
-        id:4,
-        text: 'Ballet',
-        day: 'Feb 5th at 2:30pm',
-        reminder: false, 
-        },
-    
-    ])
   return (
     <>
-    {tasks.map((task) => (<h3 key={task.id}>{task.text}</h3>))}
+    {tasks.map((task, index) => (
+    <Task key={index} task={task} onDelete={onDelete} onToggle={onToggle} />
+
+        ))}
 
     </>
   )
